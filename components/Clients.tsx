5ed20fae9149acb734d1f5625f1d4d52cd20f549
ersincodes <ersin.bahar@bilgeadam.com>
2024-06-02 13:50:18 +0300

    Footer completed and client restored

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -14,7 +14,9 @@ const Clients = () => {
       </h1>
 
       <div className="flex flex-col items-center max-lg:mt-10">
-        <div className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased  items-center justify-center relative overflow-hidden">
+        <div
+          // remove bg-white dark:bg-black dark:bg-grid-white/[0.05], h-[40rem] to 30rem , md:h-[30rem] are for the responsive design
+          className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased  items-center justify-center relative overflow-hidden">
           <InfiniteMovingCards
             items={testimonials}
             direction="right"
